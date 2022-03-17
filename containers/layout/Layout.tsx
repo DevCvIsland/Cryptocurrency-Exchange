85be51917e84c0a4d6d25661d50a5fc9a8c6c7ad
Amir Sadri <amirsadriofficial@gmail.com>
2022-03-17 19:05:36 +0330

    Update

--- containers/layout/Layout.tsx
+++ containers/layout/Layout.tsx
@@ -1,14 +1,14 @@
-import Header from "../header/Header"
-import Footer from "../footer/Footer"
+import Header from '../header/Header'
+import Footer from '../footer/Footer'
 
-const Layout = ({children} : any) => {
+const Layout = ({ children }: any) => {
   return (
     <>
       <Header />
-        {children}
+      {children}
       <Footer />
     </>
-  );
-};
+  )
+}
 
 export default Layout
