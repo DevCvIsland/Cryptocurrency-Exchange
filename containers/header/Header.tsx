df0741b1b3a6c4feb743126765e479c4ffe88f4c
Amir Sadri <amirsadriofficial@gmail.com>
2022-03-24 22:41:46 +0430

    Update

--- containers/header/Header.tsx
+++ containers/header/Header.tsx
@@ -22,11 +22,7 @@ const Header = () => {
       <Menu.Item key="about">About</Menu.Item>
       <Menu.Item key="contact">Contact</Menu.Item>
       <Menu.Item key="app">Navigation Two</Menu.Item>
-      <SubMenu
-        key="SubMenu"
-        icon={<SettingOutlined />}
-        title="Navigation Three - Submenu"
-      >
+      <SubMenu key="SubMenu" title="Navigation Three - Submenu">
         <Menu.ItemGroup title="Item 1">
           <Menu.Item key="setting:1">Option 1</Menu.Item>
           <Menu.Item key="setting:2">Option 2</Menu.Item>
