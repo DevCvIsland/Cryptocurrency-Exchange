a74c65f0d0810aa03b4f7e9ebd39273434626520
Amir Sadri <amirsadriofficial@gmail.com>
2022-03-27 17:20:50 +0430

    Update

--- pages/sign-in/index.tsx
+++ pages/sign-in/index.tsx
@@ -1,4 +1,5 @@
 import { Row, Col, Form, Input, Button, Checkbox } from 'antd'
+import Link from 'next/link'
 
 const SignIn = () => {
   const onFinish = (values: any) => {
@@ -10,75 +11,98 @@ const SignIn = () => {
   }
 
   return (
-    <div>
-      Sign In Page
-      <Row justify="center" align="middle">
-        <Col>
-          {/* xs={11} sm={12} md={12} lg={12} xl={12} */}
-          <h2>Member Sign IN</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-          <Form
-            name="basic"
-            labelCol={{
-              span: 8,
-            }}
+    <Row justify="center" align="middle">
+      <Col>
+        {/* xs={11} sm={12} md={12} lg={12} xl={12} */}
+        <h2>
+          Member <span className="theme-primary-color">Sign In</span>
+        </h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        <Form
+          name="basic"
+          labelCol={{
+            span: 8,
+          }}
+          wrapperCol={{
+            span: 16,
+          }}
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your username!',
+              },
+            ]}
             wrapperCol={{
-              span: 16,
+              offset: 0,
+              span: 24,
             }}
-            initialValues={{
-              remember: true,
-            }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off"
           >
-            <Form.Item
-              name="username"
-              rules={[
-                {
-                  required: true,
-                  message: 'Please input your username!',
-                },
-              ]}
+            <Input placeholder="Username" size="large" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your password!',
+              },
+            ]}
+            wrapperCol={{
+              offset: 0,
+              span: 24,
+            }}
+          >
+            <Input.Password
+              className="input"
+              placeholder="password"
+              size="large"
+            />
+          </Form.Item>
+          <Form.Item
+            name="remember"
+            valuePropName="checked"
+            wrapperCol={{
+              offset: 0,
+              span: 24,
+            }}
+          >
+            <Checkbox className="form-item">Remember me</Checkbox>
+          </Form.Item>
+          <Form.Item
+            wrapperCol={{
+              offset: 0,
+              span: 24,
+            }}
+          >
+            <Button
+              type="primary"
+              htmlType="submit"
+              size="large"
+              block
+              className="button"
             >
-              <Input placeholder="Username" />
-            </Form.Item>
-            <Form.Item
-              name="password"
-              rules={[
-                {
-                  required: true,
-                  message: 'Please input your password!',
-                },
-              ]}
-            >
-              <Input.Password placeholder="password" />
-            </Form.Item>
-            <Form.Item
-              className="form-item"
-              name="remember"
-              valuePropName="checked"
-              wrapperCol={{
-                offset: 8,
-                span: 16,
-              }}
-            >
-              <Checkbox className="form-item">Remember me</Checkbox>
-            </Form.Item>
-            <Form.Item
-              wrapperCol={{
-                offset: 8,
-                span: 16,
-              }}
-            >
-              <Button type="primary" htmlType="submit">
-                Submit
-              </Button>
-            </Form.Item>
-          </Form>
-        </Col>
-      </Row>
-    </div>
+              Submit
+            </Button>
+          </Form.Item>
+          <div>
+            {/* eslint-disable-next-line react/no-unescaped-entities */}
+            don't have an account?
+            <Link href="/sign-up">
+              <p className="theme-primary-color">Sign Up Now</p>
+            </Link>
+          </div>
+        </Form>
+      </Col>
+    </Row>
   )
 }
 
