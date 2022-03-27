397d9ffbaad1648b4737d30f6f5c16bfb74aada8
Amir Sadri <amirsadriofficial@gmail.com>
2022-03-27 15:44:53 +0430

    Update

--- pages/sign-in/index.tsx
+++ pages/sign-in/index.tsx
@@ -1,5 +1,14 @@
+import { Row, Col } from 'antd'
+
 const SignIn = () => {
-  return <div>Sign In Page</div>
+  return (
+    <div>
+      Sign In Page
+      <Row justify="space-between" align="middle" className="logo-section">
+        <Col xs={11} sm={12} md={12} lg={12} xl={12} />
+      </Row>
+    </div>
+  )
 }
 
 export default SignIn
