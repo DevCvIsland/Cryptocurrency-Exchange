12d003815decd0019e0c22fbb2784369671cd25a
Amir Sadri <amirsadriofficial@gmail.com>
2022-03-27 16:42:42 +0430

    Update

--- pages/sign-in/index.tsx
+++ pages/sign-in/index.tsx
@@ -1,6 +1,14 @@
-import { Row, Col } from 'antd'
+import { Row, Col, Form, Input, Button, Checkbox } from 'antd'
 
 const SignIn = () => {
+  const onFinish = (values: any) => {
+    console.log('Success:', values)
+  }
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo)
+  }
+
   return (
     <div>
       Sign In Page
@@ -9,6 +17,65 @@ const SignIn = () => {
           {/* xs={11} sm={12} md={12} lg={12} xl={12} */}
           <h2>Member Sign IN</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          <Form
+            name="basic"
+            labelCol={{
+              span: 8,
+            }}
+            wrapperCol={{
+              span: 16,
+            }}
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <Form.Item
+              name="username"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your username!',
+                },
+              ]}
+            >
+              <Input placeholder="Username" />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your password!',
+                },
+              ]}
+            >
+              <Input.Password placeholder="password" />
+            </Form.Item>
+            <Form.Item
+              className="form-item"
+              name="remember"
+              valuePropName="checked"
+              wrapperCol={{
+                offset: 8,
+                span: 16,
+              }}
+            >
+              <Checkbox className="form-item">Remember me</Checkbox>
+            </Form.Item>
+            <Form.Item
+              wrapperCol={{
+                offset: 8,
+                span: 16,
+              }}
+            >
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
         </Col>
       </Row>
     </div>
