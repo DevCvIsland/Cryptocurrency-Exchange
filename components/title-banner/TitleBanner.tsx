be1e48c363bf92e3862e11f8fe61ebca140a5518
Amir Sadri <amirsadriofficial@gmail.com>
2022-03-28 18:23:27 +0430

    Update

--- components/title-banner/TitleBanner.tsx
+++ components/title-banner/TitleBanner.tsx
@@ -7,9 +7,9 @@ type TitleBannerProps = {
 
 const TitleBanner = ({ title }: TitleBannerProps) => {
   return (
-    <div className="title-banner-section">
+    <div className="flex-row title-banner-section">
       {/* <Image src={Background} alt="Background" width={1550} height={200} /> */}
-      <h2>{title}</h2>
+      <h2 className="title-banner-text">{title}</h2>
     </div>
   )
 }
