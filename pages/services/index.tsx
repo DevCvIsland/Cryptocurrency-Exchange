b006ec8b91806a80ba66eb9066739f310158ba91
Amir Sadri <amirsadriofficial@gmail.com>
2022-03-28 19:20:16 +0430

    Update

--- pages/services/index.tsx
+++ pages/services/index.tsx
@@ -1,10 +1,15 @@
 import Layout from '../../containers/layout/Layout'
 import TitleBanner from '../../components/title-banner/TitleBanner'
 
+const ServiceContainer = () => {
+  return <div>hello</div>
+}
+
 const Services = () => {
   return (
     <Layout>
       <TitleBanner firstTitle="Our" lastTitle="Services" />
+      <ServiceContainer />
     </Layout>
   )
 }
