ce139907d95837fa6818f852a6f92e8d10076104
Amir Sadri <amirsadriofficial@gmail.com>
2022-03-28 17:49:19 +0430

    Update

--- components/title-banner/TitleBanner.tsx
+++ components/title-banner/TitleBanner.tsx
@@ -1,3 +1,6 @@
+import Image from 'next/image'
+import Background from '../../assets/root/bg-banner.jpg'
+
 type TitleBannerProps = {
   title: string
 }
@@ -5,6 +8,7 @@ type TitleBannerProps = {
 const TitleBanner = ({ title }: TitleBannerProps) => {
   return (
     <div>
+      <Image src={Background} alt="Background" width={1550} height={200} />
       <h2>{title}</h2>
     </div>
   )
