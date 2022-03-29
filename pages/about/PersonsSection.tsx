52512f156b9184c7cd05ec5d4c5b8aa8d6eae38a
Amir Sadri <amirsadriofficial@gmail.com>
2022-03-29 17:18:44 +0430

    Update

--- pages/about/PersonsSection.tsx
+++ pages/about/PersonsSection.tsx
@@ -1,27 +1,63 @@
 import { Row, Col } from 'antd'
 import Image from 'next/image'
-import One from '../../assets/person/member3.jpg'
+import MemberOne from '../../assets/person/member1.jpg'
+import MemberTwo from '../../assets/person/member2.jpg'
+import MemberThree from '../../assets/person/member3.jpg'
+import MemberFour from '../../assets/person/member4.jpg'
 
 type PersonContainerProps = {
   image: StaticImageData
+  name: string
+  direction: string
 }
 
-const PersonContainer = ({ image }: PersonContainerProps) => {
+const PersonContainer = ({ image, name, direction }: PersonContainerProps) => {
   return (
-    <Col>
-      <Image src={image} alt="Bitcoin Image" width={350} height={350} />
+    <Col
+      xs={24}
+      sm={15}
+      md={12}
+      lg={12}
+      xl={5}
+      className="flex-column person-container"
+    >
+      <Image src={image} alt="Bitcoin Image" width={265} height={330} />
+      <h3>{name}</h3>
+      <p>{direction}</p>
     </Col>
   )
 }
 
 const PersonsSection = () => {
   return (
-    <Row>
-      <div>div</div>
-      <div>
-        <PersonContainer image={One} />
-      </div>
-    </Row>
+    <>
+      <Row className="flex-column">
+        <h2>Our Experts</h2>
+        <p>A talented team of Cryptocurrency experts based in London</p>
+      </Row>
+      <Row justify="center" align="middle" className="person-section">
+        <PersonContainer
+          image={MemberOne}
+          name="Maryana Mori"
+          direction="Director"
+        />
+        <PersonContainer
+          image={MemberTwo}
+          name="Marco Verratti"
+          direction="CEO"
+        />
+        <PersonContainer
+          image={MemberThree}
+          name="Emilia Bella"
+          direction="Bitcoin Consultant"
+        />
+        <PersonContainer
+          image={MemberFour}
+          name="Antonio Conte"
+          direction="Bitcoin Developer"
+        />
+      </Row>
+    </>
   )
 }
 
