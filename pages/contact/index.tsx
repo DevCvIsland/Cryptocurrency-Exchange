5af19a99dbcef3e0a88f8803213642f6f03f3ad5
Amir Sadri <amirsadriofficial@gmail.com>
2022-03-30 14:28:04 +0430

    Update

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,13 +1,30 @@
-import { Row, Col, Button } from 'antd'
+import { Row, Col, Form, Input, Button } from 'antd'
+import {
+  HomeFilled,
+  PhoneFilled,
+  MessageFilled,
+  ProfileFilled,
+  InstagramFilled,
+  FacebookFilled,
+  TwitterSquareFilled,
+  GooglePlusSquareFilled,
+} from '@ant-design/icons'
 import Layout from '../../containers/layout/Layout'
 import TitleBanner from '../../components/title-banner/TitleBanner'
 
 const Contact = () => {
+  const onFinish = (values: any) => {
+    console.log('Success:', values)
+  }
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo)
+  }
+
   return (
     <Layout>
       <TitleBanner firstTitle="Get In" lastTitle="Touch" />
       <Row>
-        <Col xs={12} sm={10} md={8} lg={8} xl={12}>
+        <Col xs={12} sm={10} md={8} lg={8} xl={18}>
           <Row>
             <Col xs={12} sm={10} md={8} lg={8} xl={24}>
               <h3>Feel Free To Drop Us A Message</h3>
@@ -19,7 +36,103 @@ const Contact = () => {
             </Col>
           </Row>
           <Row>
-            <Col xs={12} sm={10} md={8} lg={8} xl={12}>
+            <Form
+              name="basic"
+              labelCol={{
+                span: 8,
+              }}
+              wrapperCol={{
+                span: 16,
+              }}
+              initialValues={{
+                remember: true,
+              }}
+              onFinish={onFinish}
+              onFinishFailed={onFinishFailed}
+              autoComplete="off"
+            >
+              <Form.Item
+                name="subject"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your subject!',
+                  },
+                ]}
+                wrapperCol={{
+                  offset: 0,
+                  span: 24,
+                }}
+              >
+                <Input placeholder="Subject" size="large" className="input" />
+              </Form.Item>
+              <Form.Item
+                name="Email"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your Email!',
+                  },
+                ]}
+                wrapperCol={{
+                  offset: 0,
+                  span: 24,
+                }}
+              >
+                <Input placeholder="Email" size="large" className="input" />
+              </Form.Item>
+              <Form.Item
+                name="firstname"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your firstname!',
+                  },
+                ]}
+                wrapperCol={{
+                  offset: 0,
+                  span: 24,
+                }}
+              >
+                <Input
+                  placeholder="First Name"
+                  size="large"
+                  className="input"
+                />
+              </Form.Item>
+              <Form.Item
+                name="lastname"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your lastname!',
+                  },
+                ]}
+                wrapperCol={{
+                  offset: 0,
+                  span: 24,
+                }}
+              >
+                <Input placeholder="Last name" size="large" className="input" />
+              </Form.Item>
+              <Form.Item
+                wrapperCol={{
+                  offset: 0,
+                  span: 24,
+                }}
+              >
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  size="large"
+                  block
+                  className="button"
+                >
+                  Submit
+                </Button>
+              </Form.Item>
+            </Form>
+            {/* <Col xs={12} sm={10} md={8} lg={8} xl={12}>
               subject
             </Col>
             <Col xs={12} sm={10} md={8} lg={8} xl={12}>
@@ -36,11 +149,61 @@ const Contact = () => {
             </Col>
             <Col xs={12} sm={10} md={8} lg={8} xl={24}>
               <Button>Button</Button>
-            </Col>
+            </Col> */}
           </Row>
         </Col>
-        <Col xs={12} sm={10} md={8} lg={8} xl={12}>
-          side
+        <Col xs={12} sm={10} md={8} lg={8} xl={6}>
+          <Row>
+            <Col>
+              <HomeFilled />
+            </Col>
+            <Col>
+              <p>Address</p>
+              <p>123 Disney Street Slim Av. Brooklyn Bridge, NY, New York</p>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <PhoneFilled />
+            </Col>
+            <Col>
+              <p>Phone Numbers</p>
+              <p>+88 0123 4567 890</p>
+              <p>+88 0231 3421 453</p>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <MessageFilled />
+            </Col>
+            <Col>
+              <p>Email Addresses</p>
+              <p>contact@example.com</p>
+              <p>info@example.com</p>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <ProfileFilled />
+            </Col>
+            <Col>
+              <p>socail</p>
+              <Row>
+                <p>
+                  <InstagramFilled />
+                </p>
+                <p>
+                  <FacebookFilled />
+                </p>
+                <p>
+                  <TwitterSquareFilled />
+                </p>
+                <p>
+                  <GooglePlusSquareFilled />
+                </p>
+              </Row>
+            </Col>
+          </Row>
         </Col>
       </Row>
     </Layout>
