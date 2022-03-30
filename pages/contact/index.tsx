292f2addba7f51893ab68d9aa79474525378e0b8
Amir Sadri <amirsadriofficial@gmail.com>
2022-03-30 20:27:23 +0430

    Update

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -23,7 +23,7 @@ const Contact = () => {
     <Layout>
       <TitleBanner firstTitle="Get In" lastTitle="Touch" />
       <Row justify="center" align="top">
-        <Col xs={24} sm={24} md={15} lg={12} xl={12} className="contact-form">
+        <Col xs={23} sm={20} md={20} lg={13} xl={12} className="contact-form">
           <Row justify="center" align="middle">
             <Col
               xs={24}
@@ -151,7 +151,7 @@ const Contact = () => {
                   <Input.TextArea rows={5} placeholder="Message" />
                 </Form.Item>
               </Col>
-              <Col xs={24} sm={24} md={24} lg={24} xl={24}>
+              <Col xs={24} sm={12} md={12} lg={12} xl={12}>
                 <Form.Item
                   wrapperCol={{
                     offset: 0,
@@ -172,12 +172,19 @@ const Contact = () => {
             </Row>
           </Form>
         </Col>
-        <Col xs={24} sm={24} md={15} lg={10} xl={6} className="contact-info">
+        <Col xs={23} sm={20} md={15} lg={9} xl={6} className="contact-info">
           <Row justify="space-between" align="top">
             <Col xs={3} sm={3} md={3} lg={3} xl={2}>
               <HomeFilled className="contact-info-icon" />
             </Col>
-            <Col xs={21} sm={21} md={21} lg={21} xl={20}>
+            <Col
+              xs={20}
+              sm={21}
+              md={21}
+              lg={21}
+              xl={20}
+              className="contact-info-text"
+            >
               <p>Address</p>
               <p>123 Disney Street Slim Av. Brooklyn Bridge, NY, New York</p>
             </Col>
@@ -187,7 +194,7 @@ const Contact = () => {
               <PhoneFilled className="contact-info-icon" />
             </Col>
             <Col
-              xs={21}
+              xs={20}
               sm={21}
               md={21}
               lg={21}
@@ -203,7 +210,14 @@ const Contact = () => {
             <Col xs={3} sm={3} md={3} lg={3} xl={2}>
               <MessageFilled className="contact-info-icon" />
             </Col>
-            <Col xs={21} sm={21} md={21} lg={21} xl={20}>
+            <Col
+              xs={20}
+              sm={21}
+              md={21}
+              lg={21}
+              xl={20}
+              className="contact-info-text"
+            >
               <p>Email Addresses</p>
               <p>contact@example.com</p>
               <p>info@example.com</p>
@@ -213,7 +227,14 @@ const Contact = () => {
             <Col xs={3} sm={3} md={3} lg={3} xl={2}>
               <ProfileFilled className="contact-info-icon" />
             </Col>
-            <Col xs={21} sm={21} md={21} lg={21} xl={20}>
+            <Col
+              xs={20}
+              sm={21}
+              md={21}
+              lg={21}
+              xl={20}
+              className="contact-info-text"
+            >
               <p>Social Profiles</p>
               <Row className="social-profiles-icon">
                 <FacebookFilled />
