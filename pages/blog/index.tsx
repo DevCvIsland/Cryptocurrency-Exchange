a9557c5a27359f95571ad0eae99b9a97bde21f2c
Amir Sadri <amirsadriofficial@gmail.com>
2022-03-31 20:38:27 +0430

    Update

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,4 +1,4 @@
-import { Row, Col } from 'antd'
+import { Row, Col, Pagination } from 'antd'
 import Layout from '../../containers/layout/Layout'
 import TitleBanner from '../../components/title-banner/TitleBanner'
 import BlogSidebar from './BlogSidebar'
@@ -12,11 +12,15 @@ const Blog = () => {
         <Col xs={23} sm={20} md={20} lg={13} xl={12}>
           <BlogPost />
           <BlogPost />
+          <Pagination current={2} total={50} />
         </Col>
         <Col xs={23} sm={20} md={15} lg={9} xl={6}>
           <BlogSidebar />
         </Col>
       </Row>
+      {/* <Row justify="center" align="middle">
+        <Pagination defaultCurrent={2} current={2} total={50} />
+      </Row> */}
     </Layout>
   )
 }
