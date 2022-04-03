8551161c2dcd6e3a762b644548665f20a61cc6ff
Amir Sadri <amirsadriofficial@gmail.com>
2022-04-03 15:11:22 +0430

    Update

--- pages/blog/[SingleBlogPost].tsx
+++ pages/blog/[SingleBlogPost].tsx
@@ -1,17 +1,23 @@
 import { Row, Col } from 'antd'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 import Layout from '../../containers/layout/Layout'
 import TitleBanner from '../../components/title-banner/TitleBanner'
 import BlogSidebar from './BlogSidebar'
-import image from '../../assets/blog/blog-post-1.jpg'
+import Posts from './BlogPostsData'
 
-const SingleBlogPost = () => {
+const SinglePost = () => {
+  const router = useRouter()
+  const { SingleBlogPost } = router.query
+  console.log(SingleBlogPost)
+  const post = Posts.find((obj) => obj.id === SingleBlogPost)
+
   return (
     <Layout>
       <TitleBanner firstTitle="Blog" lastTitle="Posts" />
       <Row justify="center" align="top">
         <Col xs={23} sm={20} md={20} lg={13} xl={12}>
-          <Image src={image} alt="Blog Image" />
+          <Image src={post.id} alt="Blog Image" />
           <p className="blog-post-description">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere
             eaque quasi asperiores dicta! Excepturi, exercitationem! Quas,
@@ -40,4 +46,4 @@ const SingleBlogPost = () => {
   )
 }
 
-export default SingleBlogPost
+export default SinglePost
