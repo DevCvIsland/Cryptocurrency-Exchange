d58831ed6c2ca6f5ca062fd044c0b6db50d1b2f7
Amir Sadri <amirsadriofficial@gmail.com>
2022-04-03 14:57:54 +0430

    Update

--- pages/blog/BlogPostsData.tsx
+++ pages/blog/BlogPostsData.tsx
@@ -5,17 +5,17 @@ import PostImage3 from '../../assets/blog/blog-post-3.jpg'
 const BlogPostsData = [
   {
     id: 'first-post',
-    image: PostImage1,
+    img: PostImage1,
     title: 'How Cryptocurrency Begun and Its Impact To Financial Transactions',
   },
   {
     id: 'second-post',
-    image: PostImage2,
+    img: PostImage2,
     title: 'Cryptocurrency - Who Are Involved With It? Words about members',
   },
   {
     id: 'third-post',
-    image: PostImage3,
+    img: PostImage3,
     title: 'Risks & Rewards Of Investing In Bitcoin. Pros and Cons',
   },
 ]
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,11 +1,9 @@
 import { Row, Col, Pagination } from 'antd'
 import Layout from '../../containers/layout/Layout'
 import TitleBanner from '../../components/title-banner/TitleBanner'
-import BlogSidebar from './BlogSidebar'
 import BlogPost from '../../components/blog-post/BlogPost'
-import PostImage1 from '../../assets/blog/blog-post-1.jpg'
-import PostImage2 from '../../assets/blog/blog-post-2.jpg'
-import PostImage3 from '../../assets/blog/blog-post-3.jpg'
+import Post from './BlogPostsData'
+import BlogSidebar from './BlogSidebar'
 
 const Blog = () => {
   return (
@@ -13,21 +11,9 @@ const Blog = () => {
       <TitleBanner firstTitle="Blog" lastTitle="Posts" />
       <Row justify="center" align="top">
         <Col xs={23} sm={20} md={20} lg={13} xl={12}>
-          <BlogPost
-            id="first-post"
-            title="How Cryptocurrency Begun and Its Impact To Financial Transactions"
-            image={PostImage1}
-          />
-          <BlogPost
-            id="second-post"
-            title="Cryptocurrency - Who Are Involved With It? Words about members"
-            image={PostImage2}
-          />
-          <BlogPost
-            id="third-post"
-            title="Risks & Rewards Of Investing In Bitcoin. Pros and Cons"
-            image={PostImage3}
-          />
+          <BlogPost id={Post[0].id} title={Post[0].title} image={Post[0].img} />
+          <BlogPost id={Post[1].id} title={Post[1].title} image={Post[1].img} />
+          <BlogPost id={Post[2].id} title={Post[2].title} image={Post[2].img} />
           <Pagination total={50} className="blog-pagination" />
         </Col>
         <Col xs={23} sm={20} md={15} lg={9} xl={6}>
