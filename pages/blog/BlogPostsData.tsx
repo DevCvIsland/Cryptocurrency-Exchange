16bc80a7b575ee55ba24205664ef663bc3527046
Amir Sadri <amirsadriofficial@gmail.com>
2022-04-03 14:46:29 +0430

    Update

---
+++ pages/blog/BlogPostsData.tsx
@@ -0,0 +1,23 @@
+import PostImage1 from '../../assets/blog/blog-post-1.jpg'
+import PostImage2 from '../../assets/blog/blog-post-2.jpg'
+import PostImage3 from '../../assets/blog/blog-post-3.jpg'
+
+const BlogPostsData = [
+  {
+    id: 'first-post',
+    image: PostImage1,
+    title: 'How Cryptocurrency Begun and Its Impact To Financial Transactions',
+  },
+  {
+    id: 'second-post',
+    image: PostImage2,
+    title: 'Cryptocurrency - Who Are Involved With It? Words about members',
+  },
+  {
+    id: 'third-post',
+    image: PostImage3,
+    title: 'Risks & Rewards Of Investing In Bitcoin. Pros and Cons',
+  },
+]
+
+export default BlogPostsData
