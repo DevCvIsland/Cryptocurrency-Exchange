b3ee19e65d9789d340cc1153ed2a418b34fac907
Amir Sadri <amirsadriofficial@gmail.com>
2022-04-05 12:37:30 +0430

    Update

--- components/blog-post/BlogPost.tsx
+++ components/blog-post/BlogPost.tsx
@@ -34,28 +34,6 @@ const BlogPost = ({ id, title, image }: BlogPostProps) => {
           </Button>
         </Link>
       </Col>
-      <Row className="blog-post-footer">
-        <Col>
-          <UserOutlined />
-          <p>admin</p>
-        </Col>
-        <Col>
-          <HistoryOutlined />
-          <p> 9 November 2021</p>
-        </Col>
-        <Col>
-          <CommentOutlined />
-          <p>18 comments</p>
-        </Col>
-        <Col>
-          <LinkOutlined />
-          <p>permalink</p>
-        </Col>
-        <Col>
-          <TagsOutlined />
-          <p>market, cryptocurrency, trading</p>
-        </Col>
-      </Row>
     </Row>
   )
 }
