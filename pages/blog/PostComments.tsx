a129683903bdaa429e50dee72ffedada8f9f9a5f
Amir Sadri <amirsadriofficial@gmail.com>
2022-04-06 18:52:12 +0430

    Update

--- pages/blog/PostComments.tsx
+++ pages/blog/PostComments.tsx
@@ -1,5 +1,14 @@
+import { Row, Col } from 'antd'
+
 const PostComments = () => {
-  return <div>Comments</div>
+  return (
+    <>
+      <h3>18 Comments</h3>
+      <Row>
+        <Col>ss</Col>
+      </Row>
+    </>
+  )
 }
 
 export default PostComments
