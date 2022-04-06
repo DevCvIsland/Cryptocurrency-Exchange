7aaf699f2a022851375869a8dbb17366bcccf44d
Amir Sadri <amirsadriofficial@gmail.com>
2022-04-06 20:06:36 +0430

    Update

--- pages/blog/PostComments.tsx
+++ pages/blog/PostComments.tsx
@@ -2,30 +2,43 @@ import { Row, Col, Button } from 'antd'
 import Image from 'next/image'
 import user1 from '../../assets/person/user1.jpg'
 import user2 from '../../assets/person/user2.jpg'
+import admin from '../../assets/person/user-admin.jpg'
 
 type CommentContainerProps = {
   img: StaticImageData
+  name: string
+  date: string
 }
 
-const CommentContainer = ({ img }: CommentContainerProps) => {
+const CommentContainer = ({ img, name, date }: CommentContainerProps) => {
+  const CommentContainerClasses = `comment-container ${
+    // eslint-disable-next-line eqeqeq
+    img == admin && 'admin-comment'
+  }`
+
   return (
-    <Row className="comment-container">
-      <Col>
-        <Image src={img} alt="Blog Image" className="comment-image" />
+    <Row className={CommentContainerClasses}>
+      <Col xs={4}>
+        <Image
+          src={img}
+          alt="User Image"
+          height={80}
+          className="comment-image"
+        />
       </Col>
-      <Col>
+      <Col xs={20}>
         <Row>
-          {/* <Col> */}
-          <Row justify="space-between" align="middle">
-            <Col xs={10}>name</Col>
-            <Col xs={10}>date</Col>
-          </Row>
-          {/* </Col> */}
+          <Col xs={24} className="comment-header">
+            <h4>{name}</h4>
+            <p>{date}</p>
+          </Col>
           <Col xs={24}>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error,
-            officiis consectetur accusantium sequi optio atque sunt mollitia
-            praesentium molestiae eum ea debitis, blanditiis dolores dicta
-            veritatis eius ullam. Eos, quibusdam!
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error,
+              officiis consectetur accusantium sequi optio atque sunt mollitia
+              praesentium molestiae eum ea debitis, blanditiis dolores dicta
+              veritatis eius ullam. Eos, quibusdam!
+            </p>
           </Col>
           <Col xs={24}>
             <Button type="link" className="theme-primary-color">
@@ -42,8 +55,9 @@ const PostComments = () => {
   return (
     <>
       <h3>18 Comments</h3>
-      <CommentContainer img={user1} />
-      <CommentContainer img={user2} />
+      <CommentContainer img={user1} name="Lina Marzouki" date="July 17, 2017" />
+      <CommentContainer img={admin} name="Slim Hamdi" date="July 18, 2017" />
+      <CommentContainer img={user2} name="Rawia Chniti" date="March 29, 2017" />
       <p>load More...</p>
     </>
   )
