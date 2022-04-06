941ceaaa02d0e45800b7584a4b07c5ae680f97ab
Amir Sadri <amirsadriofficial@gmail.com>
2022-04-06 19:10:48 +0430

    Update

--- pages/blog/PostComments.tsx
+++ pages/blog/PostComments.tsx
@@ -1,22 +1,29 @@
 import { Row, Col } from 'antd'
 
+const CommentContainer = () => {
+  return (
+    <Row className="comment-container">
+      <Col>image</Col>
+      <Col>
+        <Row>
+          <Row justify="space-between" align="middle">
+            <Col>name</Col>
+            <Col>date</Col>
+          </Row>
+          <Col xs={24}>text</Col>
+          <Col xs={24}>button</Col>
+        </Row>
+      </Col>
+    </Row>
+  )
+}
+
 const PostComments = () => {
   return (
     <>
       <h3>18 Comments</h3>
-      <Row className="comment-container">
-        <Col>image</Col>
-        <Col>
-          <Row>
-            <Row>
-              <Col>name</Col>
-              <Col>date</Col>
-            </Row>
-            <Col xs={24}>text</Col>
-            <Col xs={24}>button</Col>
-          </Row>
-        </Col>
-      </Row>
+      <CommentContainer />
+      <CommentContainer />
       <p>load More...</p>
     </>
   )
