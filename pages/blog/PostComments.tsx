773d4e0b9ffc95d7f36375170440ac1f1c64f7ac
Amir Sadri <amirsadriofficial@gmail.com>
2022-04-06 19:31:36 +0430

    Update

--- pages/blog/PostComments.tsx
+++ pages/blog/PostComments.tsx
@@ -11,7 +11,7 @@ const CommentContainer = ({ img }: CommentContainerProps) => {
   return (
     <Row className="comment-container">
       <Col>
-        <Image src={img} alt="Blog Image" />
+        <Image src={img} alt="Blog Image" className="comment-image" />
       </Col>
       <Col>
         <Row>
@@ -21,7 +21,12 @@ const CommentContainer = ({ img }: CommentContainerProps) => {
             <Col xs={10}>date</Col>
           </Row>
           {/* </Col> */}
-          <Col xs={24}>text</Col>
+          <Col xs={24}>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error,
+            officiis consectetur accusantium sequi optio atque sunt mollitia
+            praesentium molestiae eum ea debitis, blanditiis dolores dicta
+            veritatis eius ullam. Eos, quibusdam!
+          </Col>
           <Col xs={24}>
             <Button type="link" className="theme-primary-color">
               Link Button
