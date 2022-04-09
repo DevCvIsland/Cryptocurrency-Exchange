c0ea877d90c3684f8af3e2c7deabe46edac8dc71
Amir Sadri <amirsadriofficial@gmail.com>
2022-04-09 19:31:29 +0430

    Update

--- pages/blog/AddComment.tsx
+++ pages/blog/AddComment.tsx
@@ -1,5 +1,88 @@
+import { Form, Input, Button } from 'antd'
+import Link from 'next/link'
+
 const AddComment = () => {
-  return <div>AddComment</div>
+  const onFinish = (values: any) => {
+    console.log('Success:', values)
+  }
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo)
+  }
+
+  return (
+    <>
+      <h3>Add Comment</h3>
+      <Form
+        name="basic"
+        labelCol={{
+          span: 8,
+        }}
+        wrapperCol={{
+          span: 16,
+        }}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          name="fullname"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your fullname!',
+            },
+          ]}
+          wrapperCol={{
+            offset: 0,
+            span: 24,
+          }}
+        >
+          <Input placeholder="Full Name" size="large" className="input" />
+        </Form.Item>
+        <Form.Item
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your username!',
+            },
+          ]}
+          wrapperCol={{
+            offset: 0,
+            span: 24,
+          }}
+        >
+          <Input placeholder="Username" size="large" className="input" />
+        </Form.Item>
+        <Form.Item
+          wrapperCol={{
+            offset: 0,
+            span: 24,
+          }}
+        >
+          <Button
+            type="primary"
+            htmlType="submit"
+            size="large"
+            block
+            className="button"
+          >
+            Post
+          </Button>
+        </Form.Item>
+        <div className="flex-row sign-in-text-style">
+          {/* eslint-disable-next-line react/no-unescaped-entities */}
+          Already have an account ?
+          <Link href="/sign-in">
+            <p className="theme-primary-color">Sign In</p>
+          </Link>
+        </div>
+      </Form>
+    </>
+  )
 }
 
 export default AddComment
