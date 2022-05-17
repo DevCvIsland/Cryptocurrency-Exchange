71a94ac4ea29ee3d164dd1a8eec70613d07928a5
Amir Sadri <amirsadriofficial@gmail.com>
2022-05-17 18:17:42 +0430

    Implement need help section

--- pages/home/NeedHelp.tsx
+++ pages/home/NeedHelp.tsx
@@ -1,5 +1,21 @@
+import { Row, Col } from 'antd'
+
 const NeedHelp = () => {
-  return <div>Need Help</div>
+  return (
+    <Row>
+      <Col xs={20}>
+        <h3>Need Help?</h3>
+      </Col>
+      <Col xs={6}>
+        <p>icon</p>
+        <div>
+          <p>title</p>
+          <p>description</p>
+          <p>button</p>
+        </div>
+      </Col>
+    </Row>
+  )
 }
 
 export default NeedHelp
