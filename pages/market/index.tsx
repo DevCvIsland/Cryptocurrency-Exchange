749d355771f30109067a76a9e50681379f8536e4
Amir Sadri <amirsadriofficial@gmail.com>
2022-05-20 23:30:12 +0430

    Create markets page

--- pages/market/index.tsx
+++ pages/market/index.tsx
@@ -1,3 +1,4 @@
+import { Row, Col } from 'antd'
 import Layout from '../../containers/layout/Layout'
 import TitleBanner from '../../components/title-banner/TitleBanner'
 
@@ -5,6 +6,10 @@ const Market = () => {
   return (
     <Layout>
       <TitleBanner firstTitle="Market" lastTitle="Crypto" />
+      <Row>
+        <Col>header</Col>
+        <Col>table</Col>
+      </Row>
     </Layout>
   )
 }
