b21915e79890c9cc0962c9ddedf6a1059ce91ac5
Amir Sadri <amirsadriofficial@gmail.com>
2022-05-22 20:05:50 +0430

    Edit market table header

--- pages/market/index.tsx
+++ pages/market/index.tsx
@@ -1,4 +1,4 @@
-import { Row, Col } from 'antd'
+import { Row, Col, Button } from 'antd'
 import Layout from '../../containers/layout/Layout'
 import TitleBanner from '../../components/title-banner/TitleBanner'
 import MarketTable from './MarketTable'
@@ -9,7 +9,31 @@ const Market = () => {
       <TitleBanner firstTitle="Market" lastTitle="Crypto" />
       <Row justify="space-around" className="Markets-section">
         <Col xs={10}>
-          <h2>All Cryptos</h2>
+          {/* <h2>All Cryptos</h2> */}
+          {/* <Button type="link" size="large" className="market-table-button">
+            All
+          </Button>{' '}
+          <Button type="link" size="large" className="market-table-button">
+            Metaverse
+          </Button>{' '}
+          <Button type="link" size="large" className="market-table-button">
+            NFT
+          </Button>
+          <Button type="link" size="large" className="market-table-button">
+            Gaming
+          </Button> */}
+          <Button size="large" className="button">
+            All
+          </Button>{' '}
+          <Button size="large" className="button-outline">
+            Metaverse
+          </Button>{' '}
+          <Button size="large" className="button-outline">
+            NFT
+          </Button>{' '}
+          <Button size="large" className="button-outline">
+            Gaming
+          </Button>
         </Col>
         <Col xs={5}>
           <h2>search box</h2>
