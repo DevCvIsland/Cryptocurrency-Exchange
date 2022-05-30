1c200a2a1868404fbca180df11ac2f693d078c2e
Amir Sadri <amirsadriofficial@gmail.com>
2022-05-31 01:37:47 +0430

    Update single coin file

--- pages/market/[SingleCoin].tsx
+++ pages/market/[SingleCoin].tsx
@@ -7,7 +7,9 @@ const SingleCoin = () => {
     <Layout>
       <TitleBanner lastTitle="Bitcoin" />
       <Row justify="center">
-        <Col xs={20}>header</Col>
+        <Col xs={20}>
+          <div>title</div>
+        </Col>
         <Col xs={10}>Chart</Col>
         <Col xs={10}>
           <p>header</p>
