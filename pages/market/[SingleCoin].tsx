34636a19a44111d54a285bfd8e9892862492fe3c
Amir Sadri <amirsadriofficial@gmail.com>
2022-06-03 00:48:10 +0430

    Start styling tag of single coin

--- pages/market/[SingleCoin].tsx
+++ pages/market/[SingleCoin].tsx
@@ -1,4 +1,4 @@
-import { Row, Col } from 'antd'
+import { Row, Col, Button } from 'antd'
 import Layout from '../../containers/layout/Layout'
 import TitleBanner from '../../components/title-banner/TitleBanner'
 
@@ -13,10 +13,18 @@ const SingleCoin = () => {
         </Col>
         <Col xs={10}>Chart</Col>
         <Col xs={10}>
-          <p>header</p>
-          <div> coin sply and money</div>
-          <div>bay button</div>
-          <div>sell button</div>
+          <p>Bitcoin Price Calculater</p>
+          <div>
+            <p>Inventory: 55.2154</p>
+            <p>Qty: 1.2548</p>
+            <p>Price: 30000.11</p>
+          </div>
+          <Button size="large" className="">
+            Buy
+          </Button>
+          <Button size="large" className="">
+            Sell
+          </Button>
         </Col>
       </Row>
     </Layout>
