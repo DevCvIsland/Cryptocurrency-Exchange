8fe6fc23f9f35db18e6eedb62ef39fdbed652097
Amir Sadri <amirsadriofficial@gmail.com>
2022-06-03 21:44:48 +0430

    Update single coin

--- pages/market/[SingleCoin].tsx
+++ pages/market/[SingleCoin].tsx
@@ -12,17 +12,24 @@ const SingleCoin = () => {
           <div>price</div>
         </Col>
         <Col xs={10}>Chart</Col>
-        <Col xs={10} className="detail-single-coin">
+        <Col xs={6} className="detail-single-coin">
           <h4>Bitcoin Price Calculater</h4>
           <div>
-            <p>Inventory: 55.2154</p>
-            <p>Qty: 1.2548</p>
-            <p>Price: 30000.11</p>
+            <div>
+              <p>Inventory:</p>
+              <p>Qty:</p>
+              <p>Price:</p>
+            </div>
+            <div>
+              <p>55.2154</p>
+              <p>1.2548</p>
+              <p>30000.11</p>
+            </div>
           </div>
-          <Button size="large" className="" block>
+          <Button size="large" className="buy-button" block>
             Buy
           </Button>
-          <Button size="large" className="" block>
+          <Button size="large" className="sell-button" block>
             Sell
           </Button>
         </Col>
