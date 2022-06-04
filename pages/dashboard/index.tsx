18b632f72da8014288db9b941b41d887a88e3be2
Amir Sadri <amirsadriofficial@gmail.com>
2022-06-04 22:05:08 +0430

    Add and edit drawer

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,5 +1,33 @@
+// import { Row, Col, Menu } from 'antd'
+import React from 'react'
+import { Drawer } from 'antd'
+// import type { DrawerProps } from 'antd/es/drawer'
+
 const Dashboard = () => {
-  return <div>Dashboard</div>
+  // const [visible, setVisible] = useState(true)
+  // const onClose = () => {
+  //   setVisible(false)
+  // }
+  return (
+    <Drawer
+      title="Amir Sadri Dashboard"
+      placement="left"
+      // onClose={onClose}
+      visible
+      // extra={
+      //   <Space>
+      //     <Button onClick={onClose}>Cancel</Button>
+      //     <Button type="primary" onClick={onClose}>
+      //       OK
+      //     </Button>
+      //   </Space>
+      // }
+    >
+      <h4>Some contents...</h4>
+      <h4>Some contents...</h4>
+      <h4>Some contents...</h4>
+    </Drawer>
+  )
 }
 
 export default Dashboard
