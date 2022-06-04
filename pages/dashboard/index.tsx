d1c3df17be0bcc5478f9f1eb441a51a4b67dcf45
Amir Sadri <amirsadriofficial@gmail.com>
2022-06-05 00:32:31 +0430

    Update

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,13 +1,4 @@
-// import { Row, Col, Menu } from 'antd'
-import React from 'react'
-import { Drawer } from 'antd'
-// import type { DrawerProps } from 'antd/es/drawer'
-
 const Dashboard = () => {
-  // const [visible, setVisible] = useState(true)
-  // const onClose = () => {
-  //   setVisible(false)
-  // }
   return <p>Dashboard</p>
 }
 
