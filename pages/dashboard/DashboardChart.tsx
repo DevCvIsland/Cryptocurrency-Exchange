776a1b50c1993df6084c4e2b00a4c519c950b70d
Amir Sadri <amirsadriofficial@gmail.com>
2022-06-06 01:00:28 +0430

    Add dashboard chart

--- pages/dashboard/DashboardChart.tsx
+++ pages/dashboard/DashboardChart.tsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Line } from '@ant-design/plots'
 
 const DashboardChart = () => {
-  return <div>DashboardChart</div>
+  const [data, setData] = useState([])
+  const asyncFetch = () => {
+    fetch(
+      'https://gw.alipayobjects.com/os/bmw-prod/1d565782-dde4-4bb6-8946-ea6a38ccf184.json'
+    )
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => {
+        // eslint-disable-next-line no-console
+        console.log('fetch data failed', error)
+      })
+  }
+  useEffect(() => {
+    asyncFetch()
+  }, [])
+  const config = {
+    data,
+    padding: 'auto',
+    xField: 'Date',
+    yField: 'scales',
+    xAxis: {
+      // type: 'timeCat',
+      tickCount: 5,
+    },
+    smooth: true,
+  }
+
+  return <Line {...config} />
 }
 
 export default DashboardChart
