e0a973669fb13c30829e09c594d2259153bf5880
Amir Sadri <amirsadriofficial@gmail.com>
2022-06-05 14:04:35 +0430

    Handle layout of dashboard

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,5 +1,50 @@
-const Dashboard = () => {
-  return <p>Dashboard</p>
-}
+import React from 'react'
+import { Layout, Menu, Row, Col } from 'antd'
+// import {
+//   UploadOutlined,
+//   UserOutlined,
+//   VideoCameraOutlined,
+// } from '@ant-design/icons'
+
+const {
+  // Header, Content, Footer,
+  Sider,
+} = Layout
+
+const Dashboard = () => (
+  <Row>
+    <Col xs={4}>
+      <Sider
+        breakpoint="sm"
+        collapsedWidth="0"
+        onBreakpoint={(broken) => {
+          // eslint-disable-next-line no-console
+          console.log(broken)
+        }}
+        onCollapse={(collapsed, type) => {
+          // eslint-disable-next-line no-console
+          console.log(collapsed, type)
+        }}
+      >
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+          <Menu.Item>sssss</Menu.Item>
+          <Menu.Item>sssss</Menu.Item>
+          <Menu.Item>sssss</Menu.Item>
+          <Menu.Item>sssss</Menu.Item>
+          <Menu.Item>sssss</Menu.Item>
+          <Menu.Item>sssss</Menu.Item>
+        </Menu>
+      </Sider>
+    </Col>
+    <Col xs={20}>
+      <Row>
+        <Col xs={10}>a</Col>
+        <Col xs={10}>a</Col>
+        <Col xs={10}>a</Col>
+        <Col xs={10}>a</Col>
+      </Row>
+    </Col>
+  </Row>
+)
 
 export default Dashboard
