5a23fd57eff447637e7b27ca2fe1f3defb8d64e5
Amir Sadri <amirsadriofficial@gmail.com>
2022-06-08 23:55:54 +0430

    Update

--- pages/dashboard/RingProgress.tsx
+++ pages/dashboard/RingProgress.tsx
@@ -37,7 +37,7 @@ const RingProgressComponent = () => {
           fontSize: '12px',
           lineHeight: '14px',
         },
-        formatter: () => 'Trade',
+        formatter: () => 'Spot',
       },
     },
   }
