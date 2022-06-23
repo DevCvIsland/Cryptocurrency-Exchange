e5d7e17082ebc1ab97c5604294bc947e8d0b329f
Amir Sadri <amirsadriofficial@gmail.com>
2022-06-24 01:52:03 +0430

    implement data in table

--- pages/market/MarketTable.tsx
+++ pages/market/MarketTable.tsx
@@ -1,5 +1,6 @@
 import { Table, Button } from 'antd'
 import Link from 'next/link'
+import { useGetCryptosQuery } from '../../services/CryptoApi'
 
 const columns = [
   {
@@ -34,26 +35,49 @@ const columns = [
   },
 ]
 
-const dataSource = [
-  {
-    key: '1',
-    name: 'Bitcoin',
-    price: '$308',
-    changeOf24h: '+2.25%',
-    volumeOf24h: '30.545322M',
-    marketCap: '$50,404M',
-    action: (
-      // eslint-disable-next-line @next/next/link-passhref
-      <Link href="/market/bitcoin">
-        <Button type="link" size="large" className="market-table-button">
-          Detail / Trade
-        </Button>
-      </Link>
-    ),
-  },
-]
-
 const MarketTable = () => {
+  const { data } = useGetCryptosQuery({})
+  // eslint-disable-next-line no-console
+  // console.log('data', data.data)
+  // console.log('data name', data.data[0].name)
+  // console.log('data price', data.data[0].quote.USD.price)
+
+  const dataSource = data
+    ? data.data.map((coin: any) => ({
+        key: `${coin.id}`,
+        name: `${coin.name}`,
+        price: `${coin.quote.USD.price}`,
+        changeOf24h: `${coin.quote.USD.percent_change_24h}`,
+        volumeOf24h: `${coin.quote.USD.volume_24h}`,
+        marketCap: `${coin.quote.USD.market_cap}`,
+        action: (
+          // eslint-disable-next-line @next/next/link-passhref
+          <Link href="/market/bitcoin">
+            <Button type="link" size="large" className="market-table-button">
+              Detail / Trade
+            </Button>
+          </Link>
+        ),
+      }))
+    : [
+        {
+          key: '1',
+          name: 'Bitcoin',
+          price: '$308',
+          changeOf24h: '+2.25%',
+          volumeOf24h: '30.545322M',
+          marketCap: '$50,404M',
+          action: (
+            // eslint-disable-next-line @next/next/link-passhref
+            <Link href="/market/bitcoin">
+              <Button type="link" size="large" className="market-table-button">
+                Detail / Trade
+              </Button>
+            </Link>
+          ),
+        },
+      ]
+
   return (
     <Table
       dataSource={dataSource}
