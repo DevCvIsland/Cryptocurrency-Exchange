db55d22342393f33cf8a278c33247d4b15a6f06d
Amir Sadri <amirsadriofficial@gmail.com>
2022-06-24 02:44:30 +0430

    Change style of coin name & symbol

--- pages/market/MarketTable.tsx
+++ pages/market/MarketTable.tsx
@@ -41,7 +41,13 @@ const MarketTable = () => {
   const dataSource = data
     ? data.data.map((coin: any) => ({
         key: `${coin.id}`,
-        name: `${coin.name} / ${coin.symbol}`,
+        name: (
+          <>
+            <p className="table-coin-name">{coin.name}</p>
+            <span className="table-coin-symbol">/ {coin.symbol}</span>
+          </>
+        ),
+        // name: `${coin.name} / ${coin.symbol}`,
         price: `${coin.quote.USD.price}`,
         changeOf24h: `${coin.quote.USD.percent_change_24h}`,
         volumeOf24h: `${coin.quote.USD.volume_24h}`,
