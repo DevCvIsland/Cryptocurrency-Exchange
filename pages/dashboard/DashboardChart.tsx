fcdc7a66af17dd77d13bc250f373b3afa40a0d76
Amir Sadri <amirsadriofficial@gmail.com>
2022-06-26 03:43:12 +0430

    Implement dashboard chart data

--- pages/dashboard/DashboardChart.tsx
+++ pages/dashboard/DashboardChart.tsx
@@ -1,22 +1,8 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { Line } from '@ant-design/plots'
+import data from './ChartData'
 
 const DashboardChart = () => {
-  const [data, setData] = useState([])
-  const asyncFetch = () => {
-    fetch(
-      'https://gw.alipayobjects.com/os/bmw-prod/1d565782-dde4-4bb6-8946-ea6a38ccf184.json'
-    )
-      .then((response) => response.json())
-      .then((json) => setData(json))
-      .catch((error) => {
-        // eslint-disable-next-line no-console
-        console.log('fetch data failed', error)
-      })
-  }
-  useEffect(() => {
-    asyncFetch()
-  }, [])
   const config = {
     data,
     height: 275,
