83217ada7074f2e44d84ea31737b6ed4c23f022b
Amir Sadri <amirsadriofficial@gmail.com>
2022-06-27 04:46:50 +0430

    Update all page title of head

--- pages/blog/[SingleBlogPost].tsx
+++ pages/blog/[SingleBlogPost].tsx
@@ -8,6 +8,7 @@ import BlogPostFooter from '../../components/blog-post/BlogPostFooter'
 import PostComments from './PostComments'
 import AddComment from './AddComment'
 import BlogSidebar from './BlogSidebar'
+import Head from '../../containers/head/Head'
 
 const SinglePost = () => {
   const router = useRouter()
@@ -15,40 +16,44 @@ const SinglePost = () => {
   const post = Posts.find((obj) => obj.id === SingleBlogPost) || Posts[0]
 
   return (
-    <Layout>
-      <TitleBanner firstTitle={post.mark} lastTitle="Post" />
-      <Row justify="center" align="top">
-        <Col xs={23} sm={20} md={20} lg={13} xl={12} className="single-post">
-          <h3>{post.title}</h3>
-          <Image src={post.img} alt="Blog Image" />
-          <p className="blog-post-description">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere
-            eaque quasi asperiores dicta! Excepturi, exercitationem! Quas,
-            adipisci est facilis quam culpa doloremque dolore nemo iste
-            reprehenderit harum maiores magni. Provident?
-            <br />
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere
-            eaque quasi asperiores dicta! Excepturi, exercitationem! Quas,
-            adipisci est facilis quam culpa doloremque dolore nemo iste
-            reprehenderit harum maiores magni. Lorem ipsum dolor sit amet
-            consectetur, adipisicing elit. Facere eaque quasi asperiores dicta!
-            Excepturi, exercitationem! Quas, adipisci est facilis quam culpa
-            doloremque dolore nemo iste reprehenderit harum maiores magni.
-            <br />
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere
-            eaque quasi asperiores dicta! Excepturi, exercitationem! Quas,
-            adipisci est facilis quam culpa doloremque dolore nemo iste
-            reprehenderit harum maiores magni.
-          </p>
-          <BlogPostFooter />
-          <PostComments />
-          <AddComment />
-        </Col>
-        <Col xs={23} sm={20} md={15} lg={9} xl={6}>
-          <BlogSidebar />
-        </Col>
-      </Row>
-    </Layout>
+    <>
+      <Head title={`${post.mark} Post`} />
+      <Layout>
+        <TitleBanner firstTitle={post.mark} lastTitle="Post" />
+        <Row justify="center" align="top">
+          <Col xs={23} sm={20} md={20} lg={13} xl={12} className="single-post">
+            <h3>{post.title}</h3>
+            <Image src={post.img} alt="Blog Image" />
+            <p className="blog-post-description">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere
+              eaque quasi asperiores dicta! Excepturi, exercitationem! Quas,
+              adipisci est facilis quam culpa doloremque dolore nemo iste
+              reprehenderit harum maiores magni. Provident?
+              <br />
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere
+              eaque quasi asperiores dicta! Excepturi, exercitationem! Quas,
+              adipisci est facilis quam culpa doloremque dolore nemo iste
+              reprehenderit harum maiores magni. Lorem ipsum dolor sit amet
+              consectetur, adipisicing elit. Facere eaque quasi asperiores
+              dicta! Excepturi, exercitationem! Quas, adipisci est facilis quam
+              culpa doloremque dolore nemo iste reprehenderit harum maiores
+              magni.
+              <br />
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere
+              eaque quasi asperiores dicta! Excepturi, exercitationem! Quas,
+              adipisci est facilis quam culpa doloremque dolore nemo iste
+              reprehenderit harum maiores magni.
+            </p>
+            <BlogPostFooter />
+            <PostComments />
+            <AddComment />
+          </Col>
+          <Col xs={23} sm={20} md={15} lg={9} xl={6}>
+            <BlogSidebar />
+          </Col>
+        </Row>
+      </Layout>
+    </>
   )
 }
 
--- pages/market/[SingleCoin].tsx
+++ pages/market/[SingleCoin].tsx
@@ -10,6 +10,7 @@ import {
   useGetCryptosQuery,
   useGetCryptosInfoQuery,
 } from '../../services/CryptoApi'
+import Head from '../../containers/head/Head'
 
 const antIcon = (
   <LoadingOutlined spin style={{ fontSize: 75, color: '#f3ba2f' }} />
@@ -38,156 +39,161 @@ const SingleCoinPage = () => {
   }
 
   return (
-    <Layout>
-      <TitleBanner
-        lastTitle={coinData ? coinData.data[coinId].name : 'Loading...'}
-      />
-      <Row justify="center">
-        {coinData ? (
-          <>
-            <Col
-              xs={23}
-              sm={18}
-              md={18}
-              lg={18}
-              xl={18}
-              className="button-list-single-coin"
-            >
-              <Button size="large" className="button">
-                Overview
-              </Button>{' '}
-              <Button size="large" className="button-outline">
-                Market
-              </Button>{' '}
-              <Button size="large" className="button-outline">
-                Historical Data
-              </Button>{' '}
-              <Button size="large" className="button-outline">
-                Holders
-              </Button>{' '}
-              <Button size="large" className="button-outline">
-                Project Info
-              </Button>{' '}
-              <Button size="large" className="button-outline">
-                Wallets
-              </Button>{' '}
-              <Button size="large" className="button-outline">
-                News
-              </Button>{' '}
-              <Button size="large" className="button-outline">
-                Socials
-              </Button>{' '}
-              <Button size="large" className="button-outline">
-                Ratings
-              </Button>{' '}
-              <Button size="large" className="button-outline">
-                Analysis
-              </Button>{' '}
-              <Button size="large" className="button-outline">
-                Price Estimates
-              </Button>
-            </Col>
-            <Col
-              xs={23}
-              sm={18}
-              md={18}
-              lg={18}
-              xl={18}
-              className="info-section-single-coin"
-            >
-              <div className="info-header-single-coin">
-                <div className="flex-row">
-                  {/* eslint-disable-next-line @next/next/no-img-element */}
-                  <img
-                    src={`https://s2.coinmarketcap.com/static/img/coins/64x64/${coinId}.png`}
-                    alt={`${coinData.data[coinId].slug} logo`}
-                  />
-                  <h4>{coinData.data[coinId].name}</h4>
-                  <span>{coinData.data[coinId].symbol}</span>
-                </div>
-                <div className="price-info-single-coin">
-                  <p>
-                    $
-                    {moreCoinInfo && moreCoinInfo[0].quote.USD.price > 1
-                      ? moreCoinInfo[0].quote.USD.price.toFixed(2)
-                      : moreCoinInfo[0].quote.USD.price.toFixed(6)}
-                  </p>
-                  <div
-                    className={`${
-                      moreCoinInfo[0].quote.USD.percent_change_24h >= 0
-                        ? 'positive-background-color'
-                        : 'negative-background-color'
-                    }`}
-                  >
-                    {moreCoinInfo[0].quote.USD.percent_change_24h >= 0
-                      ? `+${moreCoinInfo[0].quote.USD.percent_change_24h.toFixed(
-                          2
-                        )}`
-                      : moreCoinInfo[0].quote.USD.percent_change_24h.toFixed(2)}
-                    %
+    <>
+      <Head title={coinData ? coinData.data[coinId].name : 'Loading...'} />
+      <Layout>
+        <TitleBanner
+          lastTitle={coinData ? coinData.data[coinId].name : 'Loading...'}
+        />
+        <Row justify="center">
+          {coinData ? (
+            <>
+              <Col
+                xs={23}
+                sm={18}
+                md={18}
+                lg={18}
+                xl={18}
+                className="button-list-single-coin"
+              >
+                <Button size="large" className="button">
+                  Overview
+                </Button>{' '}
+                <Button size="large" className="button-outline">
+                  Market
+                </Button>{' '}
+                <Button size="large" className="button-outline">
+                  Historical Data
+                </Button>{' '}
+                <Button size="large" className="button-outline">
+                  Holders
+                </Button>{' '}
+                <Button size="large" className="button-outline">
+                  Project Info
+                </Button>{' '}
+                <Button size="large" className="button-outline">
+                  Wallets
+                </Button>{' '}
+                <Button size="large" className="button-outline">
+                  News
+                </Button>{' '}
+                <Button size="large" className="button-outline">
+                  Socials
+                </Button>{' '}
+                <Button size="large" className="button-outline">
+                  Ratings
+                </Button>{' '}
+                <Button size="large" className="button-outline">
+                  Analysis
+                </Button>{' '}
+                <Button size="large" className="button-outline">
+                  Price Estimates
+                </Button>
+              </Col>
+              <Col
+                xs={23}
+                sm={18}
+                md={18}
+                lg={18}
+                xl={18}
+                className="info-section-single-coin"
+              >
+                <div className="info-header-single-coin">
+                  <div className="flex-row">
+                    {/* eslint-disable-next-line @next/next/no-img-element */}
+                    <img
+                      src={`https://s2.coinmarketcap.com/static/img/coins/64x64/${coinId}.png`}
+                      alt={`${coinData.data[coinId].slug} logo`}
+                    />
+                    <h4>{coinData.data[coinId].name}</h4>
+                    <span>{coinData.data[coinId].symbol}</span>
+                  </div>
+                  <div className="price-info-single-coin">
+                    <p>
+                      $
+                      {moreCoinInfo && moreCoinInfo[0].quote.USD.price > 1
+                        ? moreCoinInfo[0].quote.USD.price.toFixed(2)
+                        : moreCoinInfo[0].quote.USD.price.toFixed(6)}
+                    </p>
+                    <div
+                      className={`${
+                        moreCoinInfo[0].quote.USD.percent_change_24h >= 0
+                          ? 'positive-background-color'
+                          : 'negative-background-color'
+                      }`}
+                    >
+                      {moreCoinInfo[0].quote.USD.percent_change_24h >= 0
+                        ? `+${moreCoinInfo[0].quote.USD.percent_change_24h.toFixed(
+                            2
+                          )}`
+                        : moreCoinInfo[0].quote.USD.percent_change_24h.toFixed(
+                            2
+                          )}
+                      %
+                    </div>
                   </div>
                 </div>
-              </div>
-              <p>{coinData.data[coinId].description}</p>
-            </Col>
-            <Col xs={24} sm={18} md={18} lg={18} xl={12}>
-              <Stock {...config} />
-            </Col>
-            <Col
-              xs={24}
-              sm={18}
-              md={18}
-              lg={18}
-              xl={6}
-              className="detail-single-coin"
-            >
-              <h4>Trade Calculater</h4>
-              <div>
+                <p>{coinData.data[coinId].description}</p>
+              </Col>
+              <Col xs={24} sm={18} md={18} lg={18} xl={12}>
+                <Stock {...config} />
+              </Col>
+              <Col
+                xs={24}
+                sm={18}
+                md={18}
+                lg={18}
+                xl={6}
+                className="detail-single-coin"
+              >
+                <h4>Trade Calculater</h4>
                 <div>
-                  <p>{coinData.data[coinId].name}:</p>
-                  <p>Wallet:</p>
-                  <p>Amount:</p>
-                  <p>Price:</p>
-                  <p>Total:</p>
+                  <div>
+                    <p>{coinData.data[coinId].name}:</p>
+                    <p>Wallet:</p>
+                    <p>Amount:</p>
+                    <p>Price:</p>
+                    <p>Total:</p>
+                  </div>
+                  <div>
+                    <Input
+                      defaultValue={`${coinData.data[coinId].symbol} / USDT`}
+                      readOnly
+                    />
+                    <Input defaultValue="Binance" />
+                    <Input defaultValue="100" />
+                    <Input
+                      defaultValue={
+                        moreCoinInfo && moreCoinInfo[0].quote.USD.price > 1
+                          ? `$ ${moreCoinInfo[0].quote.USD.price.toFixed(2)}`
+                          : `$ ${moreCoinInfo[0].quote.USD.price.toFixed(5)}`
+                      }
+                    />
+                    <Input
+                      defaultValue={`$ ${
+                        total > 1 ? total.toFixed(2) : total.toFixed(6)
+                      }`}
+                    />
+                  </div>
                 </div>
-                <div>
-                  <Input
-                    defaultValue={`${coinData.data[coinId].symbol} / USDT`}
-                    readOnly
-                  />
-                  <Input defaultValue="Binance" />
-                  <Input defaultValue="100" />
-                  <Input
-                    defaultValue={
-                      moreCoinInfo && moreCoinInfo[0].quote.USD.price > 1
-                        ? `$ ${moreCoinInfo[0].quote.USD.price.toFixed(2)}`
-                        : `$ ${moreCoinInfo[0].quote.USD.price.toFixed(5)}`
-                    }
-                  />
-                  <Input
-                    defaultValue={`$ ${
-                      total > 1 ? total.toFixed(2) : total.toFixed(6)
-                    }`}
-                  />
-                </div>
-              </div>
-              <Button size="large" className="buy-button" block>
-                Buy
-              </Button>
-              <Button size="large" className="sell-button" block>
-                Sell
-              </Button>
-            </Col>
-          </>
-        ) : (
-          <Spin
-            className="single-coin-loading-spinner"
-            size="large"
-            indicator={antIcon}
-          />
-        )}
-      </Row>
-    </Layout>
+                <Button size="large" className="buy-button" block>
+                  Buy
+                </Button>
+                <Button size="large" className="sell-button" block>
+                  Sell
+                </Button>
+              </Col>
+            </>
+          ) : (
+            <Spin
+              className="single-coin-loading-spinner"
+              size="large"
+              indicator={antIcon}
+            />
+          )}
+        </Row>
+      </Layout>
+    </>
   )
 }
 
