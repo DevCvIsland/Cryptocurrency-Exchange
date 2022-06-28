44add61ccadf4d59a05774cc3068b59350408ab9
Amir Sadri <amirsadriofficial@gmail.com>
2022-06-29 02:48:49 +0430

    Update

--- services/CryptoApi.ts
+++ services/CryptoApi.ts
@@ -3,7 +3,9 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 const cryptoApiHeaders = {
   'X-CMC_PRO_API_KEY': 'f3002d03-f86f-4c88-86d3-9f109ab60dd4',
 }
+
 const createRequest = (url: string) => ({ url, headers: cryptoApiHeaders })
+
 export const cryptoApi = createApi({
   reducerPath: 'cryptoApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://pro-api.coinmarketcap.com' }),
@@ -16,4 +18,5 @@ export const cryptoApi = createApi({
     }),
   }),
 })
+
 export const { useGetCryptosQuery, useGetCryptosInfoQuery } = cryptoApi
