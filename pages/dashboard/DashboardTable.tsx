8ea84aa7446f1e1831507e197bebbdb3ddcd57d6
Amir Sadri <amirsadriofficial@gmail.com>
2022-06-30 05:02:33 +0430

    Add logo of coin in dashboard table

--- pages/dashboard/DashboardTable.tsx
+++ pages/dashboard/DashboardTable.tsx
@@ -27,21 +27,54 @@ const columns = [
 const dataSource = [
   {
     key: '1',
-    coin: 'Bitcoin',
+    coin: (
+      <>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src="https://s2.coinmarketcap.com/static/img/coins/32x32/1.png"
+          alt="logo"
+          className="table-logo-item"
+        />
+        <p className="table-name-item">Bitcoin</p>
+        <span className="table-symbol-item">BTC</span>
+      </>
+    ),
     amount: '2.844',
     price: '$30000.49',
     date: '2022/06/10 - 13:55',
   },
   {
     key: '2',
-    coin: 'Etherium',
+    coin: (
+      <>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src="https://s2.coinmarketcap.com/static/img/coins/32x32/2.png"
+          alt="logo"
+          className="table-logo-item"
+        />
+        <p className="table-name-item">Etherium</p>
+        <span className="table-symbol-item">/ ETH</span>
+      </>
+    ),
     amount: '7.214',
     price: '$1800.37',
     date: '2022/05/18 - 17:38',
   },
   {
     key: '3',
-    coin: 'Cardano',
+    coin: (
+      <>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src="https://s2.coinmarketcap.com/static/img/coins/32x32/5.png"
+          alt="logo"
+          className="table-logo-item"
+        />
+        <p className="table-name-item">Cardano</p>
+        <span className="table-symbol-item">/ ADA</span>
+      </>
+    ),
     amount: '10.670',
     price: '$0.750',
     date: '2022/04/15 - 21:46',
